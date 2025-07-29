ca22c2c78b057dce70278a043792d13b086c11b2
bheemdevhub <devhub@bheemverse.com>
2025-07-29 16:09:24 +0530

    chore: add and update setup.py for editable install

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='bheem-hr',
     version='1.0.0',
-    packages=find_packages(),
-    install_requires=[], # Add module-specific dependencies
+    packages=find_packages(include=["hr", "hr.*"]),
+    install_requires=[],
     include_package_data=True,
     description='Bheem Hr ERP module',
     author='Bheem Core Team',
